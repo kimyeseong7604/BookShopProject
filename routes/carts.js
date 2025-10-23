0fb6d379412eced47f5ec33afb7a74fd03008aa1
kimyesoung <kycys1834@naver.com>
2025-10-23 17:36:54 +0900

    주석 제거

--- routes/carts.js
+++ routes/carts.js
@@ -14,8 +14,4 @@ router.get('/', getCartItems);
 
 router.delete('/:id', removeCartItem);
 
-// router.get('/cart', (req,res) => {
-//     res.json('장바구니에서 선택한 상품 목록 조회');
-// });
-
 module.exports = router;
